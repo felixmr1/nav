c53fbca35f2cbe7bb66e9192ec76fc430a767970
felixmr1 <felix.rosen@einride.tech>
2025-02-01 19:06:10 -0600

    fix: save `repo_paths.csv` to correct path

--- nav.py
+++ nav.py
@@ -20,12 +20,17 @@ def clean_path(path):
     return os.path.normpath(path.strip().replace("\r", ""))
 
 
-def index_git_repos(search_dirs, output_file="repo_paths.csv"):
+def index_git_repos(search_dirs):
+    # Get the directory where the script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Create the full path for the output file
+    output_file = "repo_paths.csv"
+    output_path = os.path.join(script_dir, output_file)
+
     repo_to_path = {}
     for search_dir in search_dirs:
         if search_dir is None or not os.path.isdir(search_dir):
             raise ValueError(f"Error: '{search_dir}' is not a valid directory.")
-
         for root, dirs, _ in os.walk(search_dir):
             if ".git" in dirs:
                 repo = os.path.basename(root)
@@ -36,17 +41,16 @@ def index_git_repos(search_dirs, output_file="repo_paths.csv"):
                     if skip_dir in dirs:
                         dirs.remove(skip_dir)
 
-    with open(output_file, "w", newline="") as file:
+    # Use the full output path when opening the file
+    with open(output_path, "w", newline="") as file:
         writer = csv.writer(file)
         for key, value in repo_to_path.items():
             writer.writerow([clean_path(key), clean_path(value)])
-
     return len(repo_to_path)
 
 
 if __name__ == "__main__":
     dirs = ["~/Dev", "~/.config"]
     search_dirs = [os.path.expanduser(path) for path in dirs]
-    output_csv_file = os.path.join(os.getcwd(), "repo_paths.csv")
-    repos = index_git_repos(search_dirs, output_csv_file)
+    repos = index_git_repos(search_dirs)
     print(f"Indexed {repos} repositories. Done!")
